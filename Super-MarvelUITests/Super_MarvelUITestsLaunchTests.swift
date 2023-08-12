45a4172a08e3359cf34f39c4fc812873ad202406
Eslam Abdelmaqsoud <eslam.abdelmaqsoud@brightskiesinc.com>
2023-08-12 22:51:00 +0300

    Initial Commit

---
+++ Super-MarvelUITests/Super_MarvelUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Super_MarvelUITestsLaunchTests.swift
+//  Super-MarvelUITests
+//
+//  Created by Eslam Abdelmaqsoud on 12/08/2023.
+//
+
+import XCTest
+
+final class Super_MarvelUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
