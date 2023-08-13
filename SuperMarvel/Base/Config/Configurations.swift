2ee95557ec44be6eee2942da9927e146bfb514ed
ioslam <eslam.abdelmaqsoud@brightskiesinc.com>
2023-08-13 04:44:27 +0300

    refactor folder structures & files

---
+++ SuperMarvel/Base/Config/Configurations.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+struct Configurations {
+    
+    /// 20 items per page
+    static let pageSize = 20
+    
+    // MARK: - Base Domains
+    static let devDomain = "http://gateway.marvel.com"
+    static let stagingDomain = "http://gateway.marvel.com"
+    static let productionDomain = "http://gateway.marvel.com"
+    
+}
---
+++ SuperMarvel/Base/Config/URLs.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+struct URLs {
+    static var plainDomain: String {
+#if DEV
+        return Configurations.devDomain
+#elseif STAGE
+        return Configurations.stagingDomain
+#else
+        return Configurations.productionDomain
+#endif
+    }
+    
+    static let baseURL = URLs.plainDomain + "/v1/public/"
+}
