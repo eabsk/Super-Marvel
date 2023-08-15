12bfe26dbdb3bb3be661f02a9f272cb5acef1fd0
ioslam <eslam.abdelmaqsoud@brightskiesinc.com>
2023-08-16 00:01:25 +0300

    add characters api request

--- SuperMarvel/Networking/MoyaAPI + Extension.swift
+++ SuperMarvel/Networking/MoyaAPI + Extension.swift
@@ -20,6 +20,14 @@ extension TargetType {
         return headers
     }
     
+    var parameters: [String: Any]? {
+        [
+            "apikey": "",
+            "ts": "1691528326",
+            "hash": ""
+        ]
+    }
+    
     var sampleData: Data { return Data() }
     
 }
