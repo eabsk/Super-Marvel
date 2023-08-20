a02b30b8191d57c138813b535254e357a0ce7203
Eslam Abdelmaqsoud <133456194+eabsk@users.noreply.github.com>
2023-08-20 03:52:20 +0300

    Remove 3 warnings by swiftlint and format code lines (#25)
    * merge multi-scheme to develop (#20) (#21)
    
    * add-app-mulit-scheme
    
    * adjust app_icons in multiple-scheme
    
    * Develop into master (#23)
    
    * merge multi-scheme to develop (#20)
    
    * add-app-mulit-scheme
    
    * adjust app_icons in multiple-scheme
    
    * add fastlane setup files (#22)
    
    * fix swiftlint warnings

--- SuperMarvel/Coordinators/Base/MarvelBaseVC.swift
+++ SuperMarvel/Coordinators/Base/MarvelBaseVC.swift
@@ -115,33 +115,3 @@ class MarvelBaseVC: UIViewController {
       
     }
 }
-
-class ContextMenuButton: UIButton {
-    var previewProvider: UIContextMenuContentPreviewProvider?
-    var actionProvider: UIContextMenuActionProvider?
-
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setup()
-    }
-
-    required init?(coder: NSCoder) {
-        super.init(coder: coder)
-        setup()
-    }
-
-    func setup() {
-        let interaction = UIContextMenuInteraction(delegate: self)
-        addInteraction(interaction)
-        backgroundColor = .green
-    }
-
-    override func contextMenuInteraction(_ interaction: UIContextMenuInteraction,
-                                         configurationForMenuAtLocation location: CGPoint) -> UIContextMenuConfiguration? {
-        UIContextMenuConfiguration(
-            identifier: nil,
-            previewProvider: previewProvider,
-            actionProvider: actionProvider
-        )
-    }
-}
